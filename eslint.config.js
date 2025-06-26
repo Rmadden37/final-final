d3de50b1fedf206eede8ee09cdfceeb42a17ee6a
Rmadden37 <138617439+Rmadden37@users.noreply.github.com>
2025-06-26 16:11:44 +0000

    Your commit message here

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,14 @@
-/** @type {import('eslint').Linter.Config[]} */
-const eslintConfig = [
+
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import next from 'eslint-config-next';
+
+/** @type {import('eslint').Linter.Config} */
+export default [
+  js.configs.recommended,
+  ...next,
   {
     ignores: [
       'node_modules/',
@@ -12,7 +21,22 @@ const eslintConfig = [
       'public/',
       'build/',
     ],
-  }
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react,
+    },
+    rules: {
+      // Add or override rules as needed
+    },
+  },
 ];
-
-export default eslintConfig;
